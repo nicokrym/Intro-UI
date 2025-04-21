f04228230ce2af5062b5a24db2ae223ea6c4115e
nicokrym <167230566+nicokrym@users.noreply.github.com>
2025-04-21 12:04:56 -0300

    Terminado

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtSaludo;
+    public TMP_InputField inputNombre;
+    string nombreUsuario;
+    string saludo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtSaludo.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Saludar()
+    {
+        nombreUsuario = inputNombre.text;
+        saludo = "Hola " + nombreUsuario + "!";
+        txtSaludo.text = saludo;
+    }
+}
