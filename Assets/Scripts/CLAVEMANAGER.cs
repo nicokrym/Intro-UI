e0ec543009315d2193a826f18eead5499b8d2172
nicokrym <167230566+nicokrym@users.noreply.github.com>
2025-04-23 15:39:26 -0300

    Principio

---
+++ Assets/Scripts/CLAVEMANAGER.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CLAVEMANAGER : MonoBehaviour
+{
+    public TMP_InputField inputClave;
+    public TextMeshProUGUI txtMensaje;
+
+    string clave = "CamWard1";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtMensaje.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ComprobarClaveIngresada()
+    {
+        if(inputClave.text == clave)
+        {
+            txtMensaje.text = "Acceso concedido";
+        }
+        else
+        {
+            txtMensaje.text = "Acceso denegado";
+            inputClave.text = "";
+        }
+    }
+}
